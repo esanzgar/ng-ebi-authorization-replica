20ead361a4e6c805bbf211840c48b1b8e9f1fe79
Eduardo Sanz García <esanzgar@gmail.com>
2018-02-27 10:13:59 +0000

    test: strengthen the set of tests corrected token

--- src/app/modules/auth/auth.service.spec.ts
+++ src/app/modules/auth/auth.service.spec.ts
@@ -1,6 +1,8 @@
 import {
     TestBed,
-    inject
+    inject,
+    // fakeAsync,
+    // flushMicrotasks
 } from '@angular/core/testing';
 
 import {
@@ -22,7 +24,7 @@ import {
     DEFAULT_CONF
 } from './auth.config';
 
-describe('AuthService', () => {
+describe('AuthService (valid token)', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [{
@@ -45,4 +47,104 @@ describe('AuthService', () => {
     it('should be created', inject([AuthService], (service: AuthService) => {
         expect(service).toBeTruthy();
     }));
+
+    it('should be authenticated', inject([AuthService], (service: AuthService) => {
+        const isAuthenticated = service.isAuthenticated();
+        isAuthenticated.subscribe(result => expect(result).toBeTruthy());
+    }));
+
+    it('should have credentials', inject([AuthService], (service: AuthService) => {
+        const credentials = service.credentials();
+        credentials.subscribe(result => expect(result).toBeTruthy());
+    }));
+
+    it('should have username', inject([AuthService], (service: AuthService) => {
+        const username = service.username();
+        username.subscribe(result => expect(result).toEqual('test@ebi.ac.uk'));
+    }));
+
+    it('should have realname', inject([AuthService], (service: AuthService) => {
+        const realname = service.realname();
+        realname.subscribe(result => expect(result).toEqual('Ed Munden Gras'));
+    }));
+
+    it('should have token', inject([AuthService], (service: AuthService) => {
+        const token = service.token();
+        token.subscribe(result => expect(result).toEqual(VALID_TOKEN));
+    }));
+
+    // It doesn't work because async and timer issues
+    xit('should have log out', inject([AuthService], (service: AuthService) => {
+        service.logOut();
+        const isAuthenticated = service.isAuthenticated();
+        isAuthenticated.subscribe(result => expect(result).toBeFalsy());
+    }));
+
+    it('should be correct single sing on URL', inject([AuthService], (service: AuthService) => {
+        expect(service.getSSOURL({
+                'ttl': '30',
+                'o': '3'
+            }))
+            .toEqual('https://api.aai.ebi.ac.uk/sso?from=http%3A%2F%2Flocalhost%3A9876&ttl=30&o=3');
+    }));
+
+    it('should be correct single sing on URL', inject([AuthService], (service: AuthService) => {
+        expect(service.getSSOURL({
+                'ttl': '1441',
+                'o': '3'
+            }))
+            .toEqual('https://api.aai.ebi.ac.uk/sso?from=http%3A%2F%2Flocalhost%3A9876&ttl=1440&o=3');
+    }));
+});
+
+describe('AuthService (expired token)', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [{
+                    provide: JWT_OPTIONS,
+                    useValue: {
+                        tokenGetter: () => EXPIRED_TOKEN
+                    }
+                },
+                JwtHelperService,
+                TokenService,
+                {
+                    provide: AAP_CONFIG,
+                    useValue: DEFAULT_CONF
+                },
+                AuthService
+            ]
+        });
+    });
+
+    it('should be created', inject([AuthService], (service: AuthService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('should not be authenticated', inject([AuthService], (service: AuthService) => {
+        const isAuthenticated = service.isAuthenticated();
+        isAuthenticated.subscribe(result => expect(result).toBeFalsy());
+    }));
+
+    it('should not have credentials', inject([AuthService], (service: AuthService) => {
+        const credentials = service.credentials();
+        credentials.subscribe(result => expect(result).toBeNull());
+    }));
+
+    it('should not have username', inject([AuthService], (service: AuthService) => {
+        const username = service.username();
+        username.subscribe(result => expect(result).toBeNull());
+    }));
+
+    it('should have realname', inject([AuthService], (service: AuthService) => {
+        const realname = service.realname();
+        realname.subscribe(result => expect(result).toBeNull());
+    }));
+
+    it('should have token', inject([AuthService], (service: AuthService) => {
+        const token = service.token();
+        token.subscribe(result => expect(result).toBeNull());
+    }));
+
+
 });
--- src/app/modules/auth/token.service.spec.ts
+++ src/app/modules/auth/token.service.spec.ts
@@ -15,7 +15,7 @@ import {
     EXPIRED_TOKEN
 } from 'app/../../testing/tokens';
 
-describe('TokenService', () => {
+describe('TokenService (valid token)', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [{
@@ -32,5 +32,81 @@ describe('TokenService', () => {
 
     it('should be created', inject([TokenService], (service: TokenService) => {
         expect(service).toBeTruthy();
+    }));
+
+    it('getToken should return valid token', inject([TokenService], (service: TokenService) => {
+        expect(service.getToken()).toBe(VALID_TOKEN);
+    }));
+
+    it('token should be valid', inject([TokenService], (service: TokenService) => {
+        expect(service.isTokenValid()).toBeTruthy();
+    }));
+
+    it('token should have correct expired date', inject([TokenService], (service: TokenService) => {
+        expect(service.getTokenExpirationDate()).toEqual(new Date(1000000000000000));
+    }));
+
+    it('getClaim should return correct value', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('iss', 'Dummy')).toBe('https://tsi.ebi.ac.uk');
+    }));
+
+    it('getClaim should with non-existing claim should return default value "Dummy"', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('issr', 'Dummy')).toEqual('Dummy');
+    }));
+
+    it('getClaim should with non-existing claim should return default value null', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('issr', null)).toEqual(null);
+    }));
+
+    it('getClaim should with non-existing claim should return default value undefined', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('issr', undefined)).toEqual(undefined);
+    }));
+});
+
+describe('TokenService (expired token)', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [{
+                    provide: JWT_OPTIONS,
+                    useValue: {
+                        tokenGetter: () => EXPIRED_TOKEN
+                    }
+                },
+                JwtHelperService,
+                TokenService
+            ]
+        });
+    });
+
+    it('should be created', inject([TokenService], (service: TokenService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('getToken should return expired token', inject([TokenService], (service: TokenService) => {
+        expect(service.getToken()).toBe(EXPIRED_TOKEN);
+    }));
+
+    it('token should not be valid', inject([TokenService], (service: TokenService) => {
+        expect(service.isTokenValid()).toBeFalsy();
+    }));
+
+    it('token should have correct expired date', inject([TokenService], (service: TokenService) => {
+        expect(service.getTokenExpirationDate()).toEqual(new Date(1518083433000));
+    }));
+
+    it('getClaim should return correct value', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('iss', 'Dummy')).toBe('https://tsi.ebi.ac.uk');
+    }));
+
+    it('getClaim should with non-existing claim should return default value "Dummy"', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('issr', 'Dummy')).toEqual('Dummy');
+    }));
+
+    it('getClaim should with non-existing claim should return default value null', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('issr', null)).toEqual(null);
+    }));
+
+    it('getClaim should with non-existing claim should return default value undefined', inject([TokenService], (service: TokenService) => {
+        expect(service.getClaim('issr', undefined)).toEqual(undefined);
     }));
 });
